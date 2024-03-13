afaa8ea4ec4fa50ca1633685bb3d88d0667bd33d
Governor Clark <Governator500@vt.edu>
2024-03-13 17:35:44 -0400

    Implemented SensorQueue and created a globalVariable.h file

---
+++ Current Libraries copy/MotorDriver/MotorDriver.cpp
@@ -0,0 +1,64 @@
+#include "MotorDriver.h"
+
+void MotorDriver::setSpeed(int targetLS, int targetRS) {
+	targetLeftSpeed = targetLS;
+	targetRightSpeed = targetRS;
+}
+
+void MotorDriver::startMove() {
+		
+	// ramping
+	if (targetRightSpeed > rightSpeed)
+		rightSpeed += RAMP_RATE;
+	else if (targetRightSpeed < rightSpeed)
+		rightSpeed -= RAMP_RATE;
+
+	if (targetLeftSpeed > leftSpeed)
+		leftSpeed += RAMP_RATE;
+	else if (targetLeftSpeed < leftSpeed)
+		leftSpeed -= RAMP_RATE;
+
+	// output PWMs
+	if (rightSpeed >= 0){
+		analogWrite(RIGHT_FORWARD_PIN, rightSpeed);
+		analogWrite(RIGHT_BACKWARD_PIN, 0);
+	}
+	else{
+		analogWrite(RIGHT_FORWARD_PIN, 0);
+		analogWrite(RIGHT_BACKWARD_PIN, -rightSpeed);
+	}
+
+	if (leftSpeed >= 0){
+		analogWrite(LEFT_FORWARD_PIN, leftSpeed);
+		analogWrite(LEFT_BACKWARD_PIN, 0);
+	}
+	else{
+		analogWrite(LEFT_FORWARD_PIN, 0);
+		analogWrite(LEFT_BACKWARD_PIN, -leftSpeed);
+	}
+
+}
+	
+
+	
+void MotorDriver::FollowLine(int right, int left, double angle, double offset) {
+		
+	double ramp = .01;
+	if (offset < 5) {
+		offset = offset * 2;
+	}
+	OS = 100 * (angle) / 90 - (offset);
+	double Right = right + OS;
+	double Left = left - OS;
+
+	if(Right > 240){
+		Right = 240;
+	}
+	if(Left > 240){
+		Left = 240;
+	}
+
+	analogWrite(RIGHT_FORWARD_PIN, Right);
+	analogWrite(LEFT_FORWARD_PIN, Left);
+
+}
