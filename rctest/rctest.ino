b05d141b93edc1cf442fa358fb2c199714625dad
jboyle1013@vt.edu <58829335+jboyle1013@users.noreply.github.com>
2024-03-15 16:06:21 -0400

    Create rctest.ino

---
+++ rctest/rctest.ino
@@ -0,0 +1,13 @@
+#include "robot_control.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  RobotControl rc
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  rc.test();
+
+}
